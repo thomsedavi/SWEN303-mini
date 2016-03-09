da9cc52dcc9dcf79de47063cab3c775e746f6a8e
David Thomsen <thomsedavi@yahoo.co.nz>
2016-03-09 23:28:34 +1300

    copied code from project example, add some bootstrap example random bits

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Historical New Zealand Documents' });
+var cheerio = require('cheerio');
+var basex = require('basex');
+var client = new basex.Session("127.0.0.1", 1984, "admin", "admin");
+client.execute("open movies");
+//client.execute("xquery //movie[position() lt 10]",console.log);
+router.get("/",function(req,res,next){
+  client.execute("xquery for $movie in //movie[position() lt 10] " +
+      "return <movie> {$movie/title} {$movie/id} </movie>",
+      function (body) {
+        var $ = cheerio.load(body);
+        var list = [];
+        $('movie').each(function(i,elem){
+          var title = $(elem).find('title').text();
+          var id = $(elem).find('id').text();
+          var url = 'images/' + id + '.jpg';
+          list.push({ p: title, image: url });
+        });
+        res.render('index', { title: 'New Zealand Documents', images: list });
+      }
+  );
 });
 
 module.exports = router;
