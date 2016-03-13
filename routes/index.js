a456c78857f5220fe887f16765aef74103a894a8
David Thomsen <thomsedavi@yahoo.co.nz>
2016-03-13 19:02:37 +1300

    more learning about basex

--- routes/index.js
+++ routes/index.js
@@ -9,27 +9,21 @@ client.execute("OPEN Colenso");
 
 client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0'; " +
     "//name[@type = 'place' and position() = 1 and . = 'Manawarakau']",
-    function(err,res) {
-      if(!err) console.log(res.result);
-    }
-);
+    function(err,res) { if(!err) console.log(res.result)} );
 
-//client.execute("xquery //movie[position() lt 10]",console.log);
 router.get("/",function(req,res,next){
-  client.execute("xquery for $movie in //movie[position() lt 10] " +
-      "return <movie> {$movie/title} {$movie/id} </movie>",
-      function (body) {
-        var $ = cheerio.load(body);
-        var list = [];
-        $('movie').each(function(i,elem){
-          var title = $(elem).find('title').text();
-          var id = $(elem).find('id').text();
-          var url = 'images/' + id + '.jpg';
-          list.push({ p: title, image: url });
-        });
-        res.render('index', { title: 'New Zealand Documents', images: list });
-      }
-  );
+  client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0'; " +
+      "for $letter in //name[@type = 'place' and position() = 1 and . = 'Manawarakau'] " +
+      "return <letter> {$letter} </letter>",
+      function(aaa, bbb) {
+        var $ = cheerio.load(bbb.result);
+        $('letter').each(
+            function(i, elem) {
+              console.log('meh');
+            }
+        );
+        res.render('index', {title: 'ECS Video Rental'});
+      });
 });
 
 module.exports = router;
